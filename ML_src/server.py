979089c4d8347234f8ddac7b98bd86100e9fc2e4
Iris <xiaoyuq@google.com>
2019-02-10 22:50:07 -0500

    clean

--- ML_src/server.py
+++ ML_src/server.py
@@ -1,14 +1,12 @@
 # title
-
-from flask import Flask
-from flask import render_template
+import os
+from flask import Flask, render_template, jsonify
 app = Flask(__name__, template_folder='templates')
 app.secret = os.environ.get('SECRET')
 
 # print(title)
 # print('*********************************************************')
 # mix
-import json
 import markovify
 text_a = open("./story/nyt.txt").read()
 generator_a = markovify.Text(text_a, state_size=3)
@@ -17,23 +15,11 @@ generator_b = markovify.Text(text_b, state_size=3)
 combo = markovify.combine([generator_a, generator_b], [8, 1])
 
 from textgenrnn import textgenrnn
-textgen = textgenrnn(config_path='./NYT_2000_2019_config.json', weights_path='./NYT_2000_2019_weights.hdf5', vocab_path='./NYT_2000_2019_vocab.json')
-titles = textgen.generate(n=10, prefix="trump", return_as_list=True, temperature = 1)
+
+textgen = textgenrnn(config_path='./model/NYT_2000_2019_config.json', weights_path='./model/NYT_2000_2019_weights.hdf5', vocab_path='./model/NYT_2000_2019_vocab.json')
+titles = textgen.generate(n=100, prefix="trump", return_as_list=True, temperature = 1)
 
 from random import randint
-
-@app.after_request
-def apply_kr_hello(response):
-    """Adds some headers to all responses."""
-  
-    # Made by Kenneth Reitz. 
-    if 'MADE_BY' in os.environ:
-        response.headers["X-Was-Here"] = os.environ.get('MADE_BY')
-    
-    # Powered by Flask. 
-    response.headers["X-Powered-By"] = os.environ.get('POWERED_BY')
-    return response
-
 
 @app.route('/')
 def home():
@@ -42,7 +28,7 @@ def home():
 
 @app.route('/post')
 def post():
-    num = randint(1, 10)
+    num = randint(1, 100)
     title = titles[num]
     p1 = generator_a.make_short_sentence(140, tries=50).replace("\n", " ")
     x1 = combo.make_sentence(tries=50).replace("\n", " ")
@@ -53,7 +39,7 @@ def post():
     p3 = generator_b.make_short_sentence(100, tries=50).replace("\n", " ")
     p2 = x1 + ' ' + x2 + ' ' + x3
     outputJson = { 'title': title, 'p1': p1, 'p2': p2, 'p3': p3 }
-    output = json.dumps(outputJson)
+    output = jsonify(outputJson)
     return output
 
 if __name__ == '__main__':
