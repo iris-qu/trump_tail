b47a3a798cb5fc503e5a603bd57b8ffd201f3eaf
iris qu <iris.xiaoyu.qu@gmail.com>
2021-04-28 19:00:54 -0400

    updated scrapers

--- data_scrapers/nytScraper.py
+++ data_scrapers/nytScraper.py
@@ -1,37 +1,61 @@
 from bs4 import BeautifulSoup, SoupStrainer
 from urllib.request import urlopen
 
-# 2000-2019.txt
+# 2000-2021.txt
 # 1950-1999.txt
 # 1900-1949.txt
 # 1851-1899.txt
 
-file = open('./data/2000-2019.txt', 'w')  
+titleFile = open('./data/2019-2021_title.txt', 'w')
+contentFile = open('./data/2019-2021_content.txt', 'w')
 
-BASE_URL = "https://spiderbites.nytimes.com/"
+BASE_URL = "https://www.nytimes.com/sitemap"
 
-for year in range(2000, 2019):
-    YEAR_URL = BASE_URL + str(year) + '/articles_' + str(year) + '_' # eg. output https://spiderbites.nytimes.com/2017/articles_2017_
-    for month in range(1, 13): 
+for year in range(2019, 2022):
+    # eg. output https://www.nytimes.com/sitemap/2017
+    YEAR_URL = BASE_URL + '/' + str(year)
+    for month in range(10, 13):
         if month < 10:
             strMonth = '0' + str(month)
         else:
             strMonth = str(month)
-        MONTH_URL = YEAR_URL + strMonth + '_0000' # eg. output https://spiderbites.nytimes.com/2017/articles_2017_01_0000
-        for part in range(1, 10):
-            PART_URL = MONTH_URL + str(part) + '.html' # eg. output https://spiderbites.nytimes.com/2017/articles_2017_01_00001.html
-            print(PART_URL)
+        # eg. output https://www.nytimes.com/sitemap/2017/01
+        MONTH_URL = YEAR_URL + '/' + strMonth
+        for date in range(1, 31):
+            # eg. outputhttps://www.nytimes.com/sitemap/2017/01/31
+            if date < 10:
+                strDate = '0' + str(date)
+            else:
+                strDate = str(date)
+            DATE_URL = MONTH_URL + '/' + strDate + '/'
             try:
-                page = urlopen(PART_URL)
+                page = urlopen(DATE_URL)
             except Exception as e:
-                print(e) 
-                continue    # skip 404 errors 
-            print(PART_URL)
+                print(e)
+                continue    # skip 404 errors
             soup = BeautifulSoup(page.read(), 'html.parser')
-            ul = soup.find('ul', {'id': 'headlines'})
-            lists = ul.findChildren("li" , recursive=False)
+            ul = soup.find('ul', {'class': 'css-cmbicj'})
+            lists = ul.findChildren("li", recursive=False)
             for li in lists:
-                link = li.findChild("a", recursive=False)
-                file.write(link.text + '\n')
-file.close()
-            
+                link = li.findChild("a", recursive=False, href=True)
+                if "Trump" not in link.text:
+                    continue
+                print(DATE_URL)
+                print(link.text)
+
+                titleFile.write(link.text + '\n')
+                try:
+                    newsPage = urlopen(link['href'])
+                except Exception as e:
+                    print(e)
+                    continue    # skip 404 errors
+                newsSoup = BeautifulSoup(newsPage.read(), 'html.parser')
+                summary = newsSoup.find('p', {'id': 'article-summary'})
+                if summary:
+                    contentFile.write(summary.text + '\n')
+                newsContents = newsSoup.findAll('p', {'class': 'css-axufdj'})
+                for c in newsContents:
+                    contentFile.write(c.text + '\n')
+
+titleFile.close()
+contentFile.close()
